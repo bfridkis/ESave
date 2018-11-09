9616ea16661d0b586af01882b321723896fb7657
bfridkis <fridkisb@oregonstate.edu>
2018-11-09 12:33:19 -0600

    test

--- esave-main.js
+++ esave-main.js
@@ -36,8 +36,8 @@ app.get('/', function(req,res,next){
 	res.render('home', context)
 });
 
-
-app.use('/userTable', require('./user/user.js')("user"));
+var userRoute = require('./user/user.js');
+app.use('/userTable', userRoute("user"));
 /*
 app.use('/retailer', require('./retailer/retailer.js'));
 app.use('/product', require('./product/product.js'));
