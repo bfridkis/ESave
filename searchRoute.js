e43866d1ecfb36c287c6a9493dfac19f8747ca1c
Fridkis <Ben.J.Fridkis@p66.com>
2018-11-16 17:55:23 -0600

    corrected syntax error

--- searchRoute.js
+++ searchRoute.js
@@ -4,17 +4,18 @@ module.exports = () => {
 	var app = express();
 
 	router.get('/search', (req, res, next) => {
-	context = {};
-	context.jsscriptsSearchPage = ["sparkle.jquery.js", "search.js", "eSave.js"];
-	context.css = ["sparkle.css", "searchStyle.css"];
-	context.navbarLogo = ["images/logo.jpg"];
-	context.mainLogo = ["images/logo-medium.jpg"];
-	console.log(req.query); //*************************
-	if(req.query){
-    let searchRouteEsave = req.app.get('searchRouteEsave');
-		searchRouteEsave(req, res, context);
-	}
-	else{
-		res.render('search/search', context)
-	}
+		context = {};
+		context.jsscriptsSearchPage = ["sparkle.jquery.js", "search.js", "eSave.js"];
+		context.css = ["sparkle.css", "searchStyle.css"];
+		context.navbarLogo = ["images/logo.jpg"];
+		context.mainLogo = ["images/logo-medium.jpg"];
+		console.log(req.query); //*************************
+		if(req.query){
+	    let searchRouteEsave = req.app.get('searchRouteEsave');
+			searchRouteEsave(req, res, context);
+		}
+		else{
+			res.render('search/search', context)
+		}
+	});
 };
