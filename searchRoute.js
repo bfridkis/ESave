695cf2d7184f9730f28d241d2881aaa88ed62c6b
Fridkis <Ben.J.Fridkis@p66.com>
2018-11-16 21:00:20 -0600

    test

--- searchRoute.js
+++ searchRoute.js
@@ -13,7 +13,7 @@ module.exports = () => {
 		context.mainLogo = ["images/logo-medium.jpg"];
 		console.log(req.query); //*************************
 		if(Object.keys(req.query).length !== 0){
-			let eSaveResults = [];
+			var eSaveResults = [];
 		  for(let key in req.query){
 		    let mysql = req.app.get('mysql');
 		    if(key.charAt(0) === "p"){
