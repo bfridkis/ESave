54b798a65f9999882034cc80de3fdde5c4f17ad3
Fridkis <Ben.J.Fridkis@p66.com>
2018-11-18 11:36:28 -0600

    test

--- public/eSave.js
+++ public/eSave.js
@@ -43,7 +43,7 @@ function eSave(){
        }
        else{
          orderStageRightText.innerHTML =
-            'Click "<i class="fas fa-check-square"></i>" to ESave staged order...</span>'';
+            'Click "<i class="fas fa-check-square"></i>" to ESave staged order...</span>';
        }
       }
       else{
