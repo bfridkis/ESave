83e6fe0969c4ab67ff5a8cc1d353cdc0379d28a1
Emily Stockenbojer <stockene@oregonstate.edu>
2018-11-19 17:44:06 -0600

    working on user session cookies

--- loginroutes.js
+++ loginroutes.js
@@ -19,18 +19,19 @@ module.exports = function(app, passport) {
 
 	// process the login form
 	app.post('/login', passport.authenticate('local-login', {
-            successRedirect : '/home', // redirect to the secure profile section
-            failureRedirect : '/login', // redirect back to the signup page if there is an error
+            successRedirect : '/', // redirect to the secure home section
+            failureRedirect : '/login', // redirect back to the login page if there is an error
             failureFlash : true // allow flash messages
 		}),
         function(req, res) {
             console.log("hello");
-            // if (req.body.remember) {
-							req.session.cookie.maxAge = 3600000;
-            // } else {
-            //   req.session.cookie.expires = false;
-            // }
-        res.redirect('/home');
+
+            if (req.body.remember) {
+              req.session.cookie.maxAge = 1000 * 60 * 3;
+            } else {
+              req.session.cookie.expires = false;
+            }
+        res.redirect('/');
     });
 
 	// =====================================
