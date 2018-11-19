6ba69973dcf8ffbcbc59b62f95f0975b3b3137ea
Fridkis <Ben.J.Fridkis@p66.com>
2018-11-19 17:39:30 -0600

    added retailer.id to group by clause

--- searchRoute.js
+++ searchRoute.js
@@ -43,7 +43,7 @@ module.exports = (app) => {
 														"(promotion.min_spend <= retailer_product.price * '" + req.query[qtKey] + "' "+
 														"OR promotion.min_spend IS NULL) AND " +
 														"(promotion.qt_required <= '" + req.query[qtKey] + "' OR promotion.qt_required IS NULL) " +
-														"GROUP BY product.id ORDER BY FINAL_PRICE ASC LIMIT 1",
+														"GROUP BY retailer.id, product.id ORDER BY FINAL_PRICE ASC LIMIT 1",
 		      (err, rows, fields) => {
 		        if(err){
 		          res.write(JSON.stringify(err));
