4598052731c51ef4fa6dec3f3936e78bf4f6195f
Fridkis <Ben.J.Fridkis@p66.com>
2018-11-20 13:34:55 -0600

    moving style sheets to head in main.handlebars

--- loginroutes.js
+++ loginroutes.js
@@ -50,7 +50,7 @@ module.exports = function(app, passport) {
 	// show the signup form
 	app.get('/signup', function(req, res) {
 		// render the page and pass in any flash data if it exists
-		res.render('signup/signup', { message: req.flash('signupMessage') });
+		res.render('signup/signup', { message: req.flash('signupMessage'), css: ['loginStyle.css']});
 	});
 
 	// process the signup form
@@ -67,7 +67,8 @@ module.exports = function(app, passport) {
 	// we will use route middleware to verify this (the isLoggedIn function)
 	app.get('/home', isLoggedIn, function(req, res) {
 		res.render('home/home', {
-			user : req.user // get the user out of session and pass to template
+			user : req.user, // get the user out of session and pass to template
+			css: ['homePageStyle.css'] 
 		});
 	});
 
