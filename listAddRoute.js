a0e66997ba4ae134e2a8644cad63bb323eedd27d
Fridkis <Ben.J.Fridkis@p66.com>
2018-11-28 17:50:14 -0600

    test

--- listAddRoute.js
+++ listAddRoute.js
@@ -3,7 +3,7 @@ module.exports = (app) => {
     var router = express.Router();
     //var app = express();
 
-    router.put('/list', isLoggedIn, (req, res, next) => {
+    router.put('/', isLoggedIn, (req, res, next) => {
         var callbackCount = 0;
         //let orderDetails = JSON.parse(req.body);
         let mysql = req.app.get('mysql');
