687f84a97bdf0bafd68f85f478b7e41980a68c09
Emily Stockenbojer <stockene@oregonstate.edu>
2018-11-29 12:05:43 -0600

    adding promotions to wishlist

--- userWishListRoute.js
+++ userWishListRoute.js
@@ -5,10 +5,12 @@ module.exports = (app) => {
 	//var app = express();
 
   function getWishList(res, mysql, context, userid){
-      mysql.pool.query("SELECT order.current_price, order_product.quantity, product.name AS product, retailer.name AS retailer FROM wish_list " +
+      mysql.pool.query("SELECT order.current_price, order_product.quantity, product.name AS product, promotion.description AS promotion, retailer.name AS retailer FROM wish_list " +
       "INNER JOIN `order` ON wish_list.order = order.id " +
       "INNER JOIN order_product ON order.id = order_product.order " +
       "INNER JOIN product ON order_product.product = product.id " +
+      "INNER JOIN order_promotion ON order.id = order_promotion.order " +
+      "INNER JOIN promotion ON order_promotion.promotion = promotion.id " +
       "INNER JOIN retailer ON order.retailer = retailer.id WHERE wish_list.user = ?", userid, function(err, rows){
           if(err){
               res.write(JSON.stringify(err));
