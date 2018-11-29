9259a964d456688f21489a8cb68c7440c0748de4
Emily Stockenbojer <stockene@oregonstate.edu>
2018-11-29 03:15:31 -0600

    adding wish list page routes

--- userWishListRoute.js
+++ userWishListRoute.js
@@ -6,7 +6,7 @@ module.exports = (app) => {
 
   function getWishList(res, mysql, context, userid){
 
-      mysql.pool.query("SELECT order.current_price, order_product.quantity, product.name AS product, retailer.name AS retailer FROM wish_list INNER JOIN order ON wish_list.order = order.id INNER JOIN order_product ON order.id = order_product.order INNER JOIN product ON order_product.product = product.id INNER JOIN retailer ON order.retailer = retailer.id WHERE wish_list.user = ?", [userid], function(error, results, fields){
+      mysql.pool.query("SELECT order.current_price, order_product.quantity, product.name AS product, retailer.name AS retailer FROM wish_list INNER JOIN order ON wish_list.order = order.id INNER JOIN order_product ON order.id = order_product.order INNER JOIN product ON order_product.product = product.id INNER JOIN retailer ON order.retailer = retailer.id WHERE wish_list.user = 2", function(error, results, fields){
           if(error){
               res.write(JSON.stringify(error));
               res.end();
