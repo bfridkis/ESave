925c446eedfde80876b486942d8296bb46470d3c
Fridkis <Ben.J.Fridkis@p66.com>
2018-11-30 23:58:59 -0600

    test

--- public/promotion.js
+++ public/promotion.js
@@ -5,7 +5,7 @@ function addWishList(list, product_name, quantity, retailer_name, product, promo
 	 //Open GET request, using queryString
 
  let queryString = `/search?p1=${product_name}&q1=${quantity}&ret=${retailer_name}`;
-	 req.open("GET", "/search?p1=playstation&q1=1&ret=target", true);
+	 req.open("GET", "/search?p1=play&q1=1&ret=NULL", true);
 	 //req.setRequestHeader('Content-Type', 'application/json');  <-- Not needed for get request
 
 	 if(req.status >= 200 && req.status < 400)
