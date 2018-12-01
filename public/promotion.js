22d7bb328825124d252216d2ef125a9af0cf6299
Fridkis <Ben.J.Fridkis@p66.com>
2018-12-01 00:06:39 -0600

    test

--- public/promotion.js
+++ public/promotion.js
@@ -7,14 +7,15 @@ function addWishList(list, product_name, quantity, retailer_name, product, promo
  let queryString = `/search?p1=${product_name}&q1=${quantity}&ret=${retailer_name}`;
 	 req.open("GET", "/search?p1=play&q1=1&ret=NULL", true);
 	 //req.setRequestHeader('Content-Type', 'application/json');  <-- Not needed for get request
-
-	 if(req.status >= 200 && req.status < 400)
- {
-	 let results = JSON.parse(req.responseText);
-   console.log(results);//**********************************
-			 listAdder(list, results, promo);
- }
-	 else
- console.log("Error: " + req.statusText);
+   req.addEventListener("load", () => {
+  	 if(req.status >= 200 && req.status < 400)
+     {
+    	 let results = JSON.parse(req.responseText);
+       console.log(results);//**********************************
+    			 listAdder(list, results, promo);
+     }
+  	 else
+        console.log("Error: " + req.statusText);
+ });
 req.send(null);
 }
