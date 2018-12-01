b37c1803b505cb98d42e702db6a3c14714a43621
Fridkis <Ben.J.Fridkis@p66.com>
2018-12-01 00:41:14 -0600

    test

--- promotionRoute.js
+++ promotionRoute.js
@@ -15,7 +15,6 @@ module.exports = (app) => {
 
     mysql.pool.query("SELECT product.name AS product_name, promotion.product, retailer.name AS retailer_name, " +
 										 "promotion.discount, promotion.description, promotion.ecoupon, promotion.qt_required, promotion.id, " +
-										 //"retailer.id AS RET_ID " +
                     "FROM promotion " +
                     "INNER JOIN retailer on retailer.id=promotion.retailer " +
                     "INNER JOIN product on promotion.product=product.id", function(error, results, fields){
