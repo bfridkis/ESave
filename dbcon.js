215f286b1840b4770d5feabd9f240ffab4ea8e92
Fridkis <Ben.J.Fridkis@p66.com>
2018-12-05 12:57:50 -0600

    reverting to process.env.passowrd

--- dbcon.js
+++ dbcon.js
@@ -3,7 +3,7 @@ var pool = mysql.createPool({
   connectionLimit : 10,
   host            : 'us-cdbr-iron-east-01.cleardb.net',
   user            : 'ba1f2b48b756b7',
-  password        : 'process.env.password',
+  password        : process.env.password,
   database        : 'heroku_fa0c8c6c2b62722',
   multipleStatements : true
 });
