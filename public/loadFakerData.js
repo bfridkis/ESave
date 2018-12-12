f11b772279d8931f2d68c9b3205091b674b1b19f
Fridkis <Ben.J.Fridkis@p66.com>
2018-12-12 08:53:49 -0600

    test

--- public/loadFakerData.js
+++ public/loadFakerData.js
@@ -37,7 +37,7 @@ function loadFakerData(){
                 password: pw};
     req.addEventListener('load', () => {
       resultCount++;
-      console.log(req.responseText, req.statusText); //*****************
+      //console.log(req.responseText, req.statusText); //*****************
       if(req.status >= 200 && req.status < 400){
         let response = JSON.parse(req.responseText).Response;
         let results = document.querySelector("#results");
