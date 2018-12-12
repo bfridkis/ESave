a0e3933effc4ef08fdc44ce184a79e3ec12e12e8
Fridkis <Ben.J.Fridkis@p66.com>
2018-12-12 14:35:14 -0600

    test

--- public/loadFakerData.js
+++ public/loadFakerData.js
@@ -17,7 +17,7 @@ function loadFakerData(){
 
   function submit(e){
     console.log(e.keyCode, this);//*************
-    console.log(this === "null");//********
+    console.log(this === {"null"});//********
     if(e.keyCode === "13" || (this !== "null" && this.getAttribute("id") === "submit")){
       let prods = values[0].value,
           rets = values[1].value,
