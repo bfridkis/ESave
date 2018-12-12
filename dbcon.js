2bc13f299b64fd7d64fbb8ca0fc0720c963f6fcf
Fridkis <Ben.J.Fridkis@p66.com>
2018-12-12 17:00:01 -0600

    Increased connectionLimit

--- dbcon.js
+++ dbcon.js
@@ -1,6 +1,6 @@
 var mysql = require('mysql');
 var pool = mysql.createPool({
-  connectionLimit : 10,
+  connectionLimit : 5000,
   host            : 'us-cdbr-iron-east-01.cleardb.net',
   user            : 'ba1f2b48b756b7',
   password        : process.env.password,
