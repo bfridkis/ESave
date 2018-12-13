1cefdeeb026bf8ac8e6c2a101cf712017f73f004
Fridkis <Ben.J.Fridkis@p66.com>
2018-12-13 17:43:51 -0600

    test

--- loadFakerDataRoute.js
+++ loadFakerDataRoute.js
@@ -118,7 +118,7 @@ module.exports = (app) => {
                                                       for (let i = 0; i < 6; i++){
                                                           ecoupon += faker.random.alphaNumeric();
                                                       }
-                                                      let expirationDate = new Date(faker.date.future());
+                                                      let expirationDate = faker.date.future();
                                                       expirationDate = expirationDate.substring(0, 10);
                                                       console.log(expirationDate);//*******************************
                                                       let qt_required = getRandomInt(2) === 1 ? faker.random.number() : null;
