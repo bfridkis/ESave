8e8e7d7b01edd4da4ed0cc493d91c57c4fa320a2
Fridkis <bfridkis@gmail.com>
2018-12-21 10:02:01 -0600

    corrected variable name

--- loadFakerDataRoute.js
+++ loadFakerDataRoute.js
@@ -247,7 +247,7 @@ module.exports = (app) => {
         deleteQuery = "DELETE FROM promotion where id > '31';" +
                       "DELETE FROM retailer WHERE id > '51';" +
                       "DELETE FROM product WHERE id > '71';";
-        mysql.pool.query(selectQuery,
+        mysql.pool.query(deleteQuery,
           (err, row, fields) => {
               if (err) {
                 res.write(JSON.stringify(err));
