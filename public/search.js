411938a86e89266a3fad8b53711c44d234484ba5
Fridkis <bfridkis@gmail.com>
2018-12-21 10:45:03 -0600

    added capacity to press enter button to stage products

--- public/search.js
+++ public/search.js
@@ -9,47 +9,52 @@ var prodInput = document.getElementById("prodInput");
 var qtInput = document.getElementById("quantity");
 var addProd = document.getElementsByClassName("btn")[0];
 var stageTable = document.querySelector("#order-stage-table");
-addProd.addEventListener("click", () => {
-  if(prodInput.value !== ""){
-    let row = document.createElement("tr");
-    row.setAttribute("class", "searchItemRow");
-    stageTable.appendChild(row);
-    let prodDesc = row.appendChild(document.createElement("td"));
-    prodDesc.textContent = prodInput.value;
-    prodDesc.style.textOverflow = "ellipsis";
-    prodDesc.style.overflow = "hidden";
-    prodDesc.style.whiteSpace = "nowrap";
-    prodDesc.style.fontSize = "1.15rem";
-    prodDesc.setAttribute("class", "searchItem");
-    let prodQT = row.appendChild(document.createElement("td"));
-    prodQT.textContent = qtInput.value;
-    prodQT.style.paddingLeft = ("25px");
-    prodQT.style.textAlign = ("center");
-    prodQT.style.fontSize = "1.15rem";
-    prodQT.setAttribute("class", "qtSearchItem");
-    let increaseQT = row.appendChild(document.createElement("td"));
-    increaseQT.innerHTML = '<i class="fas fa-caret-up"></i>';
-    increaseQT.childNodes[0].style.color = "purple";
-    increaseQT.childNodes[0].style.paddingLeft = "2px";
-    increaseQT.childNodes[0].addEventListener("click", () => {
-      prodQT.textContent = String(Number(prodQT.textContent) + 1);
-    });
-    let decreaseQT = row.appendChild(document.createElement("td"));
-    decreaseQT.innerHTML = '<i class="fas fa-caret-down"></i>';
-    decreaseQT.childNodes[0].style.color = "rgb(39, 206, 100)";
-    decreaseQT.childNodes[0].addEventListener("click", () => {
-      if(Number(prodQT.textContent) >= 1){
-      prodQT.textContent = String(Number(prodQT.textContent) - 1);
-      }
-    });
-    let deleteB = row.appendChild(document.createElement("td"));
-    deleteB.innerHTML = '<i class="fas fa-minus-circle"></i>';
-    deleteB.childNodes[0].style.color = "red";
-    deleteB.childNodes[0].addEventListener("click", () => {
-      stageTable.removeChild(row);
-    });
+addProd.addEventListener("click", stageProduct.bind(addProd));
+document.addEventListener("keypress", stageProduct.bind("null"));
+
+function stageProduct(e){
+  if(e.keyCode === 13 || (String(this) !== "null" && this.getAttribute("id") === "submit")){
+    if(prodInput.value !== ""){
+      let row = document.createElement("tr");
+      row.setAttribute("class", "searchItemRow");
+      stageTable.appendChild(row);
+      let prodDesc = row.appendChild(document.createElement("td"));
+      prodDesc.textContent = prodInput.value;
+      prodDesc.style.textOverflow = "ellipsis";
+      prodDesc.style.overflow = "hidden";
+      prodDesc.style.whiteSpace = "nowrap";
+      prodDesc.style.fontSize = "1.15rem";
+      prodDesc.setAttribute("class", "searchItem");
+      let prodQT = row.appendChild(document.createElement("td"));
+      prodQT.textContent = qtInput.value;
+      prodQT.style.paddingLeft = ("25px");
+      prodQT.style.textAlign = ("center");
+      prodQT.style.fontSize = "1.15rem";
+      prodQT.setAttribute("class", "qtSearchItem");
+      let increaseQT = row.appendChild(document.createElement("td"));
+      increaseQT.innerHTML = '<i class="fas fa-caret-up"></i>';
+      increaseQT.childNodes[0].style.color = "purple";
+      increaseQT.childNodes[0].style.paddingLeft = "2px";
+      increaseQT.childNodes[0].addEventListener("click", () => {
+        prodQT.textContent = String(Number(prodQT.textContent) + 1);
+      });
+      let decreaseQT = row.appendChild(document.createElement("td"));
+      decreaseQT.innerHTML = '<i class="fas fa-caret-down"></i>';
+      decreaseQT.childNodes[0].style.color = "rgb(39, 206, 100)";
+      decreaseQT.childNodes[0].addEventListener("click", () => {
+        if(Number(prodQT.textContent) >= 1){
+        prodQT.textContent = String(Number(prodQT.textContent) - 1);
+        }
+      });
+      let deleteB = row.appendChild(document.createElement("td"));
+      deleteB.innerHTML = '<i class="fas fa-minus-circle"></i>';
+      deleteB.childNodes[0].style.color = "red";
+      deleteB.childNodes[0].addEventListener("click", () => {
+        stageTable.removeChild(row);
+      });
+    }
   }
-});
+}
 
 // References
 // https://www.w3schools.com/js/js_htmldom_elements.asp
