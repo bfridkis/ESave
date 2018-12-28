954b497d8b06079cb8e04d352f07ea4f2f1ab642
Fridkis <bfridkis@gmail.com>
2018-12-28 13:48:19 -0600

    test

--- public/eSave.js
+++ public/eSave.js
@@ -336,6 +336,7 @@ function processUnmatched(orderStageRight, orderStageRightText,
       let nextButton = pageButtonsRow.appendChild(document.createElement("td"));
       nextButton.innerHTML = '<i class="fas fa-arrow-right next"></i>';
       page === "esave-button" ? page = "0" : page = Number(page) + 1;
+      console.log(page);//***********************************************
       nextButton.firstChild.setAttribute("id", String(page));
       nextButton.firstChild.addEventListener("click", eSaveWrapper.bind(nextButton.firstChild, 500));
     }
