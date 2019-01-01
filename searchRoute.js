aabeab751357eeca9c23d77fca847e88bd65594f
Fridkis <bfridkis@gmail.com>
2019-01-01 17:11:06 -0600

    corrected method

--- searchRoute.js
+++ searchRoute.js
@@ -203,9 +203,9 @@ module.exports = app => {
 			//available.
 			function complete2(resultsTotalsByRetailer){
 				callbackCount2++;
-				console.log("resultsTotalsByRetailer length: ", resultsTotalsByRetailer.length);//*************************
-				if(resultsTotalsByRetailer.length
-						&& callbackCount2 === resultsTotalsByRetailer.length){
+				console.log("resultsTotalsByRetailer length: ", resultsTotalsByRetailer.keys.length);//*************************
+				if(resultsTotalsByRetailer.keys.length
+						&& callbackCount2 === resultsTotalsByRetailer.keys.length){
 					console.log("Discounted Final Results: ", resultsTotalsByRetailer);//***************
 					console.log("Winner :", resultsTotalsByRetailer[minFinalPriceRetailer]);//**************
 					res.send(JSON.stringify(resultsTotalsByRetailer[minFinalPriceRetailer]));
