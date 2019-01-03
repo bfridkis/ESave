1b27142bb9e4135f9fd146669613f0b3e8a6c86c
Fridkis <bfridkis@gmail.com>
2019-01-03 16:37:09 -0600

    test

--- searchRoute.js
+++ searchRoute.js
@@ -118,12 +118,14 @@ module.exports = app => {
 					eSaveResults.forEach( (productResults, i) => {
 						productResults.results.forEach( result => {
 							if(resultsTotalsByRetailer.hasOwnProperty(result.RET_NAME)){
+								//Use scaling where necessary to ensure all values are rounded to 2 decimal places
+								//See https://stackoverflow.com/questions/11832914/round-to-at-most-2-decimal-places-only-if-necessary
 								resultsTotalsByRetailer[result.RET_NAME]["discounted_price"] +=
 									Number(result.DISCOUNTED_PRICE).toFixed(2);
 								resultsTotalsByRetailer[result.RET_NAME]["discount"] +=
-									Number(result.TOTAL_DISCOUNT).toFixed(2);
+									Math.round(Number(result.TOTAL_DISCOUNT) + 0.00001) * 100 / 100;
 								resultsTotalsByRetailer[result.RET_NAME]["initial_price"] +=
-									Number(result.INITIAL_PRICE).toFixed(2);
+									Math.round(Number(result.INITIAL_PRICE) + 0.00001) * 100 / 100;
 								resultsTotalsByRetailer[result.RET_NAME]["prices"][productResults.prodNum]
 									= Number(result.PRICE_PER_UNIT);
 								resultsTotalsByRetailer[result.RET_NAME]["num_prods"]++;
@@ -140,6 +142,7 @@ module.exports = app => {
 								resultsTotalsByRetailer[result.RET_NAME]["num_prods"] = 1;
 								resultsTotalsByRetailer[result.RET_NAME]["ret_id"] = result.RET_ID;
 							}
+							console.log("What the... ", resultsTotalsByRetailer[result.RET_NAME]);//***********
 						});
 					});
 					for(retailer in resultsTotalsByRetailer){
@@ -278,4 +281,3 @@ function isLoggedIn(req, res, next) {
 // * https://www.google.com/search?q=force+a+push+heroku&rlz=1C1GGRV_enUS818US818&oq=force+a+push+heroku&aqs=chrome..69i57j0.2670j0j7&sourceid=chrome&ie=UTF-8
 // * https://stackoverflow.com/questions/2641347/short-circuit-array-foreach-like-calling-break
 // * https://stackoverflow.com/questions/11832914/round-to-at-most-2-decimal-places-only-if-necessary
-// * https://www.w3schools.com/jsref/jsref_tofixed.asp
