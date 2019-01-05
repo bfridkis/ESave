c651b1340bbad1f4bb0545d13bc1de5e130be852
Fridkis <bfridkis@gmail.com>
2019-01-05 15:46:47 -0600

    corrected syntax

--- loadFakerDataRoute.js
+++ loadFakerDataRoute.js
@@ -200,7 +200,7 @@ module.exports = (app) => {
                                                                 });
                                                               }
                                                             });
-                                                          }
+                                                          });
                                                         }
                                                     else {
                                                       res.send({
