15d89931353a06df5ff520d11685ba26c8716856
Fridkis <bfridkis@gmail.com>
2019-01-05 19:50:38 -0600

    corrected variable name

--- searchRoute.js
+++ searchRoute.js
@@ -314,7 +314,7 @@ module.exports = app => {
 		//than 999.99).
 		function stripCommas(str){
 			while(str.indexOf(",") !== -1){
-				str = str.slice(0, ip.indexOf(",")) + str.slice(indexOf(",") + 1);
+				str = str.slice(0, str.indexOf(",")) + str.slice(indexOf(",") + 1);
 			}
 			return str;
 		}
