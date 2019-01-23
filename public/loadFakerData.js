d831360c026249a2918e013989ff3b3e775b7a0a
Fridkis <bfridkis@gmail.com>
2019-01-23 15:02:41 -0600

    test

--- public/loadFakerData.js
+++ public/loadFakerData.js
@@ -106,6 +106,7 @@ function loadFakerData(){
           let results1 = document.querySelector("#results1"),
               results2 = document.querySelector("#results2");
           console.log(req.responseText);//***************************
+          console.log("Last char: ", req.responseText.substring(-1));//*************
           if(req.responseText.substring(-1) === "{"){
             req.responseText.substring(-1) = "}";
           }
