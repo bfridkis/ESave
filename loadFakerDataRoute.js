c5396c9fb928c208f37876eb8047e12a4b79ce9b
Fridkis <bfridkis@gmail.com>
2019-01-23 18:05:09 -0600

    test

--- loadFakerDataRoute.js
+++ loadFakerDataRoute.js
@@ -320,7 +320,7 @@ module.exports = (app) => {
         return p7;
       }).catch( finish => {
         console.log("FINISH: ", finish);//********************
-        console.log("???"");//********************
+        console.log("???");//********************
         if(finish !== "No promos"){
           res.write(JSON.stringify(err));
           res.status(400);
